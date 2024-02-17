66c80156bd76c8484f21955e9be29805c2a235e6
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-02-17 13:54:31 +0530

    Added the recoil root to the main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from "react-hot-toast"
+import { RecoilRoot } from 'recoil'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Toaster />
-      <App />
+      <RecoilRoot>
+        <Toaster />
+        <App />
+      </RecoilRoot>
     </BrowserRouter>
   </React.StrictMode>,
 )
